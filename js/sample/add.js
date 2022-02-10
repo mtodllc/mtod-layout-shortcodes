61dac9a600187da91a07cff1becdd29b8d684206
mtodllc <>
2022-02-10 13:39:37 +0900

    v0.0.1

---
+++ js/sample/add.js
@@ -0,0 +1,2 @@
+let element = document.getElementById('testidtext');
+element.insertAdjacentHTML('beforebegin', '<p>Javascriptで挿入しています</p>');
--- module/sample/shortcod-sample.php
+++ module/sample/shortcod-sample.php
@@ -3,26 +3,37 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 class mtod_add_class{
     public
     function __construct() {
-        add_action( 'wp_enqueue_scripts', array( $this, 'add_css_class' ) );
+        add_action( 'wp_head', array( $this, 'add_css_class' ) );
+        add_action( 'wp_enqueue_scripts', array( $this, 'add_javascript' ) );
         add_shortcode('hello', array( $this, 'hello_func' ) );
     }
+
     public
     function add_css_class(){
         global $post;
         if( has_shortcode( $post->post_content, 'hello' ) ) {
             $csscode = <<< EOD
             <style>.test001{color:#ff00ff;}</style>
+
             EOD;
             echo $csscode;
         }
     }
+
     public
+    function add_javascript(){
+        wp_enqueue_script ( 'modernizr-custom', MTODLS_URI. '/js/sample/add.js', array(), false ,true  );
+    }
+
+    public　
     function hello_func() {
+        $imguri = MTODLS_URI.'/images/sample/';
         $code = <<< EOD
-        <p class="test001">色は匂へど 散りぬるを</p>
+        <p id="testidtext" class="test001">色は匂へど 散りぬるを</p>
         <p class="test001">我が世誰ぞ 常ならむ</p>
         <p class="test001">有為の奥山 今日越えて</p>
         <p class="test001">浅き夢見じ 酔ひもせず</p>
+        <div><img src="{$imguri}sample.png"></div>
         EOD;
         return $code;
     }
